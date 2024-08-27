d04e7554a7d03ec88ce944615bc9caab96c49f10
Rien3844 <102456478+Rien3844@users.noreply.github.com>
2024-08-27 19:30:05 +0900

    clear
    개인과제 필수구현 및 도전구현 완료

--- game.js
+++ game.js
@@ -2,32 +2,47 @@ import chalk from 'chalk';
 import readlineSync from 'readline-sync';
 
 class Player {
-  constructor(hp, ap) {
+  constructor() {
     this.hp = 100;
-    this.ap = 5;
+    this.ap = 15;
   }
 
+  // 플레이어의 공격
   attack(monster = new Monster(), damage) {
-    // 플레이어의 공격
     monster.hp -= damage;
+  }
+  upstage(stage) {
+    this.hp += 15 + random(20, 40);
+    this.ap += random(1, 4);
+  }
+  cheat() {
+    return (this.hp = 9999), (this.ap = 9999);
   }
 }
 
 class Monster {
-  constructor(hp, ap) {
-    this.hp = 100;
-    this.ap = 2.5;
+  constructor(stage) {
+    this.hp = 50 + stage * random(5, 10);
+    this.ap = 7 + stage;
   }
 
+  // 몬스터의 공격
   attack(player = new Player(), damage) {
-    // 몬스터의 공격
     player.hp -= damage;
   }
 }
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function displayStatus(stage, player, monster) {
-  //player = new Player();
-  //monster = new Monster();
+  if (stage >= 2) {
+    console.log(
+      `스테이지 클리어!
+      레벨업 했습니다. 플레이어의 체력이 ${player.hp}, 공격력이 ${player.ap}로 상승하고 몬스터가 강해졌습니다.`,
+    );
+  }
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
@@ -39,8 +54,6 @@ function displayStatus(stage, player, monster) {
 
 const battle = async (stage, player, monster) => {
   let logs = [];
-  player = new Player();
-  monster = new Monster();
 
   while (player.hp > 0) {
     console.clear();
@@ -54,8 +67,7 @@ const battle = async (stage, player, monster) => {
     if (player.hp <= 0) {
       break;
     }
-
-    console.log(chalk.green(`\n1. 공격한다 2. 아무것도 하지않는다. 3. 게임 종료`));
+    console.log(chalk.green(`\n1. 공격한다 2. 연속공격(25%) 3. 방어(40%) 4. 도주(3%)`));
     const choice = readlineSync.question('당신의 선택은? ');
 
     // 플레이어의 선택에 따라 다음 행동 처리
@@ -66,21 +78,48 @@ const battle = async (stage, player, monster) => {
         player.attack(monster, player.ap);
         logs.push(`몬스터에게 ${player.ap}의 피해를 입혔습니다.`);
         monster.attack(player, monster.ap);
-        //return monster.hp;
 
         break;
       case '2':
         logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
-        logs.push(`턴을 넘깁니다.`);
-        monster.attack(player, monster.ap);
-        //return player.hp;
+        if (100 - random(0, 100) < 25) {
+          logs.push(`연속 공격 성공!`);
+          player.attack(monster, player.ap);
+          logs.push(`몬스터에게 ${player.ap}의 피해를 입혔습니다.`);
+          player.attack(monster, player.ap);
+          logs.push(`몬스터에게 ${player.ap}의 피해를 입혔습니다.`);
+        } else {
+          logs.push(`연속 공격 실패...몬스터에게 공격받습니다.`);
+          monster.attack(player, monster.ap);
+        }
         break;
       case '3':
         logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
-        logs.push(`게임 종료. ${process.exit(0)}`);
+        if (100 - random(0, 100) < 40) {
+          logs.push(`방어 성공!`);
+          player.hp += random(5, 30);
+          logs.push(`체력이 소폭 회복됩니다.`);
+        } else {
+          logs.push(`방어 실패...몬스터에게 공격받습니다.`);
+          monster.attack(player, monster.ap);
+        }
+        break;
+      case '4':
+        logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+        if (100 - random(0, 100) < 5) {
+          monster.hp = 0;
+          logs.push(`도주 성공!`);
+        } else {
+          logs.push(`도주 실패...몬스터에게 공격받습니다.`);
+          monster.attack(player, monster.ap);
+          break;
+        }
+      case '0':
+        logs.push(chalk.bgYellowBright(`치트모드 발동.`));
+        player.cheat();
         break;
       default:
-        console.log(chalk.red('올바른 선택을 하세요.'));
+        logs.push(chalk.red('올바른 선택을 하세요.'));
     }
   }
 };
@@ -93,7 +132,21 @@ export async function startGame() {
   while (stage <= 10) {
     const monster = new Monster(stage);
     await battle(stage, player, monster);
-    stage++;
+
     // 스테이지 클리어 및 게임 종료 조건
+    if (stage === 10) {
+      console.log('게임 클리어!');
+      process.exit(0);
+    }
+    if (monster.hp <= 0) {
+      console.log('몬스터 처치! 다음층으로 갑니다.');
+      stage++;
+      if (stage > 1 && player.hp > 0) {
+        player.upstage();
+      }
+    } else if (player.hp <= 0) {
+      console.log('게임오버');
+      process.exit(0);
+    }
   }
 }
